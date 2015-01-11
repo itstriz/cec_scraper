93785ad443f78fc795e058ea0566cda21c5fd96d
Matt Johnson <mjohn23@gmail.com>
2015-01-11 13:10:29 -0600

    Cleanup BS4 import

--- cec_bs4.py
+++ cec_bs4.py
@@ -5,7 +5,11 @@ BASE_URL = "http://www.emergencyclosingcenter.com/ecc/home.jsp"
 
 html = urlopen(BASE_URL).read()
 soup = BeautifulSoup(html)
-items = soup.findAll("p", {"class": "text"})
 
-for item in items:
-    print item.text
+for table in soup.findAll('table'):
+    if table.parent.name == 'td':
+        data = table
+
+for td in data.findAll('td'):
+    if td.text.strip() != "":
+        print td.text.strip()
