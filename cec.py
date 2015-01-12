3c5bed29c69767b0dc8055fdf8657586b42a16fb
Matt Johnson <mjohn23@gmail.com>
2015-01-12 12:41:29 -0600

    Cleanup and add dates

--- cec.py
+++ cec.py
@@ -1,48 +1,42 @@
-import urllib2
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+from urllib2 import urlopen
 
-def get_data(url):
-    response = urllib2.urlopen(url)
-    html = response.read()
+BASE_URL = "http://www.emergencyclosingcenter.com/ecc/home.jsp"
 
-    data_start = html.find("<td><p class=text>")
-    data_end = html.find("</table>", data_start)
-    all_results = html[data_start:data_end]
+html = urlopen(BASE_URL).read()
+soup = BeautifulSoup(html)
+
+for table in soup.findAll('table'):
+    if table.parent.name == 'td':
+        data = table
+
+closings = []
+count = 0
+for tr in data.findAll('tr'):
+    school_name = tr.findAll('td')[1].text.strip()
+    city = tr.findAll('td')[3].text.strip()
+    status = tr.findAll('td')[5].text.strip()
+    if status.find("TODAY") != -1:
+        status_date = datetime.now()
+        status_date = status_date.strftime("%Y-%m-%d")
+    elif status.find("TOMORROW") != -1:
+        status_date = datetime.now()
+        status_date = status_date + timedelta(days=+1)
+        status_date = status_date.strftime("%Y-%m-%d")
+    else:
+        status_date = None
+
+    if school_name == "":
+        school_name = closings[count - 1]['school_name']
+        city = closings[count - 1]['city']
     
-    parse_data(all_results)
+    if school_name != "Facility Name":
+        closings.append({'school_name': school_name,
+                         'city': city,
+                         'status': status,
+                         'status_date': status_date})     
+        count = count + 1
 
-    return None
-
-def get_facility_name(data, old_pos):
-    start_search = "<p class=text>"
-    end_search = "</p>"
-    start_pos = data.find(start_search, old_pos)
-    end_pos = data.find(end_search, start_pos)
-        
-    facility_name = data[start_pos + len(start_search):end_pos].strip()
-    print facility_name
-
-    while end_pos < len(data) and end_pos != -1:
-        start_pos = data.find(start_search, end_pos + 1)
-        end_pos = data.find(end_search, start_pos)
-
-        if end_pos == -1:
-            break
-
-        facility_name = data[start_pos + len(start_search):end_pos].strip()
-
-        print facility_name
-
-    return facility_name
-    
-def parse_data(data):
-    """ Parse data into a list """
-    parsed_data = []
-
-    # Find first bit of data
-    get_facility_name(data, 0)
-
-    return parsed_data
-
-if __name__ == "__main__":
-    cec_url = 'http://www.emergencyclosingcenter.com/ecc/home.jsp'
-    get_data(cec_url)
+for closing in closings:
+    print "%s: %s - %s [%s]" % (closing['school_name'], closing['city'], closing['status'], closing['status_date'])
